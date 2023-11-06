5e3bcefb08d75c504bc84011ee82ee8bd21b63bb
Prateek Chaurasia <111336976+Prateek07-Kumar@users.noreply.github.com>
2023-11-06 23:07:28 +0530

    Add files via upload

---
+++ icc_world_cup/icc_world_cup.sql
@@ -0,0 +1,26 @@
+create database icc_world_cup;
+use icc_world_cup;
+
+create table icc_world_cup
+(
+Team_1 Varchar(20),
+Team_2 Varchar(20),
+Winner Varchar(20)
+);
+
+INSERT INTO icc_world_cup values('India','SL','India');
+INSERT INTO icc_world_cup values('SL','Aus','Aus');
+INSERT INTO icc_world_cup values('SA','Eng','Eng');
+INSERT INTO icc_world_cup values('Eng','NZ','NZ');
+INSERT INTO icc_world_cup values('Aus','India','India');
+
+select * from icc_world_cup;
+select team_name, count(1) as no_of_matches_players, sum(win_flag) as no_of_matches_won, count(1) - sum(win_flag) as no_of_losses
+from (
+select Team_1 as team_name, case when team_1 = Winner then 1 else 0 end as win_flag
+from icc_world_cup
+union all 
+select Team_2, case when Team_2 = Winner then 1 else 0 end as win_flag
+from icc_world_cup) A
+group by team_name
+order by no_of_matches_won desc;
